72e35123321a32c87209e54669dc3d79d0c69967
fh17 <franharte.97@gmail.com>
2026-04-15 12:31:35 +0200

    our first code, we eddited quadcopter_env code to inport a new .usd model where we added a payload sphere to the drone by a link with a fixed drone to link joint and a primatic link to payload joint

---
+++ _2_drones_FRANCO.py
@@ -0,0 +1,24 @@
+import os
+import isaaclab.sim as sim_utils
+from isaaclab.assets import ArticulationCfg
+from isaaclab.actuators import ImplicitActuatorCfg
+
+_2_DRONES_PAYLOAD_CFG = ArticulationCfg(
+    prim_path="{ENV_REGEX_NS}/Robot",
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=os.path.abspath("./source/isaaclab_tasks/isaaclab_tasks/direct/quadcopter/Franco/assets/2x5_in_drone_with_payload.usd"),
+        activate_contact_sensors=True,
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 1.0),
+        joint_pos={".*": 0.0},
+        joint_vel={".*": 0.0},
+    ),
+    actuators={
+        "dummy": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            stiffness=0.0,
+            damping=0.0,
+        ),
+    },
+)
