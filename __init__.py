60b170eb9c0326e166b71b891fdd995bdbd5d9ec
fh17 <franharte.97@gmail.com>
2026-04-27 13:23:06 +0200

    first try with curriculum structur. we didint get results yet

--- __init__.py
+++ __init__.py
@@ -15,6 +15,21 @@ from . import agents
 # Register Gym environments.
 ##
 
+
+gym.register(
+    id="Isaac-Quadcopter-Direct-OUR_DRONES-v0",
+    entry_point=f"{__name__}.env_FRANCO_our_drone_3_curriculum:QuadcopterEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.env_FRANCO_our_drone_3_curriculum:QuadcopterEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg-FRANCO.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:QuadcopterPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
+
+
+
 #1 drone with payload
 gym.register(
     id="Isaac-Quadcopter-Direct-FRANCO-v0",
